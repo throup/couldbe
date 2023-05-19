1b0e7f750dc3b5f0a62d1fe3ec559244862ddb43
Chris Throup <chris@throup.eu>
2023-05-19 09:56:40 +0200

    Apply dependency overrides to manage vulnerabilities in transitive dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,34 @@
 object Versions {
   def cats                = "2.9.0"
+
   def catsTestkit         = "2.1.5"
+
   def scalatest           = "3.2.16"
+
   def scalatestPlus       = "3.2.14.0"
+
   def catsScalacheck      = "0.3.2"
+
   def disciplineScalatest = "2.2.0"
+
   def scalaLogging        = "3.9.5"
+
   def logback             = "1.4.7"
+
+  object Plugin {
+    // Manually update this value in project/plugins.sbt as well
+    def sbtGithubPackages = "0.5.3"
+
+    // Manually update this value in project/plugins.sbt as well
+    def sbtRelease = "1.1.0"
+  }
+  object Override {
+    def protobufJava = "[3.16.3,)"
+
+    def jacksonDatabind = "[2.13.4.2,)"
+
+    def jsoup = "[1.15.3,)"
+
+    def snakeyaml = "[2.0,)"
+  }
 }
