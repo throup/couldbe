0fd894b8ffbe67ccc71abc970e8ac84e0817f9b9
Chris Throup <chris@throup.eu>
2023-05-20 15:24:36 +0200

    Switch names of CouldBe and CouldHave

--- src/main/scala/eu/throup/couldbe/typesEq.scala
+++ src/main/scala/eu/throup/couldbe/typesEq.scala
@@ -4,26 +4,26 @@ package couldbe
 import cats.*
 
 trait EqHierarchy[A] extends Hash[A], PartialOrder[A]
-given [A: Eq: CouldBe[Hash]: CouldBe[PartialOrder]]: EqHierarchy[CouldBeGiven[A]] =
+given [A: Eq: CouldHave[Hash]: CouldHave[PartialOrder]]: EqHierarchy[CouldBeGiven[A]] =
   new EqHierarchy[CouldBeGiven[A]]:
     override def hash(x: CouldBeGiven[A]): Int =
-      CouldBe[Hash, A].act(H => x.gift.map(H.hash(_)).getOrElse(0))(() => x.hashCode())
+      CouldHave[Hash, A].act(H => x.gift.map(H.hash(_)).getOrElse(0))(() => x.hashCode())
     override def partialCompare(x: CouldBeGiven[A], y: CouldBeGiven[A]): Double =
       (x, y) match
         case (IsNotGiven, _: IsGiven[?])    => -1.0
         case (_: IsGiven[?], IsNotGiven)    => 1.0
         case (IsNotGiven, IsNotGiven) => 0.0
         case (IsGiven(a), IsGiven(b)) =>
-          CouldBe[PartialOrder, A].act(_.partialCompare(a, b))(() =>
+          CouldHave[PartialOrder, A].act(_.partialCompare(a, b))(() =>
             if Eq.eqv(a, b) then 0 else Double.NaN
           )
-given [A: Eq: CouldBe[Hash]: CouldBe[PartialOrder]]: EqHierarchy[IsGiven[A]] =
+given [A: Eq: CouldHave[Hash]: CouldHave[PartialOrder]]: EqHierarchy[IsGiven[A]] =
   new EqHierarchy[IsGiven[A]]:
     override def hash(x: IsGiven[A]): Int =
-      CouldBe[Hash, A].act(H => x.gift.map(H.hash(_)).getOrElse(0))(() => x.hashCode())
+      CouldHave[Hash, A].act(H => x.gift.map(H.hash(_)).getOrElse(0))(() => x.hashCode())
 
     override def partialCompare(x: IsGiven[A], y: IsGiven[A]): Double = {
-      CouldBe[PartialOrder, A].act(_.partialCompare(x.get, y.get))(() =>
+      CouldHave[PartialOrder, A].act(_.partialCompare(x.get, y.get))(() =>
         if Eq.eqv(x.get, y.get) then 0 else Double.NaN
       )
     }
--- src/test/scala/eu/throup/couldbe/CouldBeTest.scala
+++ src/test/scala/eu/throup/couldbe/CouldBeTest.scala
@@ -7,20 +7,20 @@ import org.scalatest.matchers.should.Matchers
 
 class CouldBeTest extends AnyFreeSpec with Matchers:
   "Prove it" in {
-    trait SomethingToBe[A]
-    given SomethingToBe[BigInt] = new SomethingToBe[BigInt]() {}
+    def willItCompile[A[_]: CouldBe[Monad]]: String =
+      CouldBe[Monad, A].act((monad: Monad[A]) => "I got a Monad!!!!\n")(() => "I got nothing!!!\n")
 
-    def willItCompile[A: CouldBe[SomethingToBe]]: String =
-      CouldBe[SomethingToBe, A].act((td: SomethingToBe[A]) => "I got a SomethingToBe!!!!\n")(() => "I got nothing!!!\n")
+    // This type definitely does not have a Monad
+    class Sam[A]
 
-    val exampleWillInt    = willItCompile[Int]
-    val exampleWillBigInt = willItCompile[BigInt]
-    val exampleWillString = willItCompile[String]
-    val exampleWill       = exampleWillInt + exampleWillBigInt + exampleWillString
+    val exampleWillInt    = willItCompile[List]
+    val exampleWillString = willItCompile[Sam]
+    val exampleWillDouble = willItCompile[Option]
+    val exampleWill       = exampleWillInt + exampleWillString + exampleWillDouble
 
     exampleWill shouldBe
-      """I got nothing!!!
-        |I got a SomethingToBe!!!!
+      """I got a Monad!!!!
         |I got nothing!!!
+        |I got a Monad!!!!
         |""".stripMargin
   }
